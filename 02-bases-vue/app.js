8fe3d0eaaed2607c8a5a6f831620243d2ec679cd
Macailh <salvadorenriquegb@gmail.com>
2023-08-27 12:28:14 -0600

    Add declarative representation

--- 02-bases-vue/app.js
+++ 02-bases-vue/app.js
@@ -1,8 +1,17 @@
 const app = Vue.createApp({
-    template:`
-    <h1>Hola Mundo</h1>
-    <p>Desde app.js</p>
-    `
+    // template:`
+    // <h1>Hello world</h1>
+    // <p>From app.js</p>
+    // <p>{{ 1 + 1 }}</p>
+    // <p>[1,2,3,4,5,6]</p>
+    // <p>{{ true ? 'Active': 'Inactive'}}</p>
+    // `
+    // methods: {},
+    // watch: {}, 
+
+    // setup() {}
+
+    
 })
 
 app.mount('#myApp')
