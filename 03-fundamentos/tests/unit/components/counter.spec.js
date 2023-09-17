8546f9f349346c92dbd4d272412cb5e4ed86cd34
Macailh <salvadorenriquegb@gmail.com>
2023-09-17 11:03:53 -0600

    Refactor counter tests

--- 03-fundamentos/tests/unit/components/counter.spec.js
+++ 03-fundamentos/tests/unit/components/counter.spec.js
@@ -2,63 +2,42 @@ import { shallowMount } from '@vue/test-utils'
 import Counter from '@/components/Counter'
 
 describe('Counter component', () => {
+  let wrapper
+
+  beforeEach(() => {
+    const startValue = 1
+    wrapper = shallowMount(Counter, {
+      props: {
+        start: startValue
+      }
+    })
+  })
   // it('snapshot match', () => {
   //   const wrapper = shallowMount(Counter)
   //   expect(wrapper.html()).toMatchSnapshot
   // })
 
   test('h2 should have the default value "Counter"', () => {
-    const startValue = 1
-    const wrapper = shallowMount(Counter, {
-      props: {
-        start: startValue
-      }
-    })
     const h2 = wrapper.find('h2')
     console.log(h2.text())
     expect(h2.text()).toBe('Counter')
   })
 
   test('the value should be 1', () => {
-    const startValue = 1
-    const wrapper = shallowMount(Counter, {
-      props: {
-        start: startValue
-      }
-    })
     // const pTags = wrapper.findAll('p')
     const value = wrapper.find('[data-test-id="counter"]').text()
     
     expect(value).toBe('1')
   })
 
-  test('should increment by 1 the counter', async() => {
-    const startValue = 1
-    const wrapper = shallowMount(Counter, {
-      props: {
-        start: startValue
-      }
-    })
-    const increaseBtn = wrapper.findAll('button')
+  test('should increment and decrement the counter', async() => {
+    const [decreaseBtn,increaseBtn] = wrapper.findAll('button')
 
-    await increaseBtn[1].trigger('click')
-    const value = wrapper.find('[data-test-id="counter"]').text()
-
-    expect(value).toBe('2')
-  })
-
-  test('should descrease by 1 the counter', async() => {
-    const startValue = 1
-    const wrapper = shallowMount(Counter, {
-      props: {
-        start: startValue
-      }
-    })
-    const decreaseBtn = wrapper.find('button')
-
+    await increaseBtn.trigger('click')
     await decreaseBtn.trigger('click')
+
     const value = wrapper.find('[data-test-id="counter"]').text()
 
-    expect(value).toBe('0')
+    expect(value).toBe('1')
   })
 })
